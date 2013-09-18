427a5b71f852330669caa97908290273154b3dab
Ribesg <ribesg@yahoo.fr>
2013-09-18 11:25:19 +0200

    Add exception package & InvalidMessageException for futur parsing

---
+++ src/main/java/fr/ribesg/alix/api/exception/InvalidMessageException.java
@@ -0,0 +1,7 @@
+package fr.ribesg.alix.api.exception;
+/**
+ * Represents an Exception thrown when a Message is incorrect.
+ *
+ * @author Ribesg
+ */
+public class InvalidMessageException extends Exception {}
