f68334d31c91dd88a3c3679a5bed98da25811ea1
Ribesg <Ribesg@yahoo.fr>
2014-03-17 22:07:03 +0100

    Add ModeIrcPacket

---
+++ src/main/java/fr/ribesg/alix/api/message/ModeIrcPacket.java
@@ -0,0 +1,80 @@
+package fr.ribesg.alix.api.message;
+import fr.ribesg.alix.api.enums.Command;
+
+/**
+ * This class allow easy build of a MODE IRC Packet.
+ */
+public class ModeIrcPacket extends IrcPacket {
+
+	/**
+	 * Used to properly call the super(...) constructor.
+	 *
+	 * @param channelName a first parameter
+	 * @param modeString  a second parameter
+	 * @param parameters  tons of other parameters
+	 *
+	 * @return a merge of all those parameters
+	 */
+	private static String[] mergeArguments(final String channelName, final String modeString, final String... parameters) {
+		final String[] result = new String[parameters.length + 2];
+		result[0] = channelName;
+		result[1] = modeString;
+		System.arraycopy(parameters, 0, result, 2, parameters.length);
+		return result;
+	}
+
+	private final String entityName;
+
+	private final String modeString;
+
+	private final String[] parameters;
+
+	/**
+	 * Short MODE Packet constructor.
+	 *
+	 * @param entityName the entity of the Mode packet i.e. a Channel name or
+	 *                   a User name
+	 */
+	public ModeIrcPacket(final String entityName) {
+		super(null, Command.MODE.name(), null, entityName);
+		this.entityName = entityName;
+		this.modeString = null;
+		this.parameters = null;
+	}
+
+	/**
+	 * Complete MODE Packet constructor.
+	 *
+	 * @param entityName the entity of the Mode packet i.e. a Channel name or
+	 *                   a User name
+	 * @param modeString a String containing mode codes/changes, eg. +o
+	 * @param parameters additional parameters for the mode
+	 */
+	public ModeIrcPacket(final String entityName, final String modeString, final String... parameters) {
+		super(null, Command.MODE.name(), null, mergeArguments(entityName, modeString, parameters));
+		this.entityName = entityName;
+		this.modeString = modeString;
+		this.parameters = parameters;
+	}
+
+	/**
+	 * @return this Mode packet Entity name
+	 */
+	public String getEntityName() {
+		return this.entityName;
+	}
+
+	/**
+	 * @return this Mode packet Mode String
+	 */
+	public String getModeString() {
+		return modeString;
+	}
+
+	/**
+	 * @return this Mode packet parameters
+	 */
+	public String[] getModeParameters() {
+		return parameters;
+	}
+}
